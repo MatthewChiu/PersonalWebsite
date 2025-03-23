42036d98c5ab0bdbda6b3fa41327c806c1ecee7d
Matt Chiu <matthewgychiu@gmail.com>
2025-03-23 19:31:30 -0400

    Quartz sync: Mar 23, 2025, 7:31 PM

--- quartz/util/og.tsx
+++ quartz/util/og.tsx
@@ -150,8 +150,10 @@ export const defaultImage: SocialImageOptions["imageStructure"] = (
 
   // I changed this
   // const iconPath = `https://${cfg.baseUrl}/static/icon.png`
+
+  const iconPath = `https://mattchiu.com/static/icon.png`
   // const iconPath = `https://mattchiu.com/content/quartz/static/icon.png`
-  const iconPath = '../static/icon.png'
+  // const iconPath = '../static/icon.png'
 
   return (
     <div
